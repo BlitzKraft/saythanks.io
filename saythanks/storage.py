b5a590ff015decab91ea271feb3f1568d4f8df7a
Kenneth Reitz <me@kennethreitz.org>
2016-11-20 17:41:53 -0500

    database first

--- saythanks/storage.py
+++ saythanks/storage.py
@@ -2,6 +2,8 @@
 
 import records
 
+db = records.Database()
+
 
 class Note(object):
     """docstring for Note"""
@@ -26,20 +28,20 @@ class Inbox(object):
     @classmethod
     def is_linked(cls, auth_id):
         q = 'SELECT * from inboxes where auth_id = :auth_id'
-        r = records.query(q, auth_id=auth_id)
+        r = db.query(q, auth_id=auth_id)
         return bool(len(r))
 
     @classmethod
     def store(cls, slug, auth_id):
         q = 'INSERT into inboxes VALUES :slug :auth_id'
-        r = records.query(q, slug=slug, auth_id=auth_id)
+        r = db.query(q, slug=slug, auth_id=auth_id)
 
         return Inbox(slug)
 
     def notes(self):
         """Returns a list of notes, ordered reverse-chronologically."""
         q = 'SELECT * from notes where inbox_id = :slug'
-        r = records.query(q, slug=self.slug)
+        r = db.query(q, slug=self.slug)
 
         notes = []
 
