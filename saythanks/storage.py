0ec4d0aab70743a6679915d83841e25a389ccbad
Kenneth Reitz <me@kennethreitz.org>
2016-11-20 17:07:58 -0500

    inbox again

--- saythanks/storage.py
+++ saythanks/storage.py
@@ -7,7 +7,6 @@ class Inbox(object):
     """docstring for Inbox"""
     def __init__(self, slug):
         self.slug = slug
-        self._auth_id = None
 
     @classmethod
     def is_linked(cls, auth_id):
@@ -19,21 +18,7 @@ class Inbox(object):
     def store(cls, slug, auth_id):
         q = 'INSERT into inboxes VALUES :slug :auth_id'
         r = records.query(q, slug=slug, auth_id=auth_id)
-        # TODO: Return Inbox instance.
-
-    @property
-    def auth_id(self):
-        pass
-
-    @auth_id.setter
-    def auth_id(self, value):
-        self._auth_id = value
-
-    def does_exist(self):
-        return bool(self.auth_id)
-
-    def create_if_not_stored(self):
-        pass
+        return Inbox(slug)
 
 # def add_note(inbox, ):
 #     pass
