127a7588c4dd14ebacd992f2c63bdf4eb602d31f
Kenneth Reitz <me@kennethreitz.org>
2016-11-20 17:20:29 -0500

    notes lookup

--- saythanks/storage.py
+++ saythanks/storage.py
@@ -3,10 +3,25 @@
 import records
 
 
+class Note(object):
+    """docstring for Note"""
+    def __init__(self):
+        self.body = None
+        self.byline = None
+        self.inbox = None
+
+    @classmethod
+    def from_inbox(inbox, body, byline)
+        self.body = body
+        self.byline = byline
+        self.inbox = Inbox(inbox)
+
+
 class Inbox(object):
     """docstring for Inbox"""
     def __init__(self, slug):
         self.slug = slug
+
 
     @classmethod
     def is_linked(cls, auth_id):
@@ -18,10 +33,18 @@ class Inbox(object):
     def store(cls, slug, auth_id):
         q = 'INSERT into inboxes VALUES :slug :auth_id'
         r = records.query(q, slug=slug, auth_id=auth_id)
+
         return Inbox(slug)
 
     def notes(self):
         """Returns a list of notes, ordered reverse-chronologically."""
         q = 'SELECT * from notes where inbox_id = :slug'
         r = records.query(q, slug=self.slug)
+
+        notes = []
+
+        for n in r:
+            note = Note.from_inbox(slug, n['body'], n['byline'])
+
+        return notes
 
