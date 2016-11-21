371b77645dcbdba921389d25cc8122278610ea0b
Kenneth Reitz <me@kennethreitz.org>
2016-11-20 21:12:31 -0500

    fix

--- saythanks/email.py
+++ saythanks/email.py
@@ -1,5 +1,33 @@
+import os
+
+import sendgrid
+from sendgrid.helpers.mail import Email, Content, Mail
+
 # Email Infrastructure
 # --------------------
 
+API_KEY = os.environ['SENDGRID_API_KEY']
+sg = sendgrid.SendGridAPIClient(apikey=API_KEY)
+
+TEMPLATE = """{}
+
+--{}
+
+=========
+This note of thanks was brought to you by SayThanks.io.
+A Kenneth Reitz (me@kennethreitz.org) project.
+"""
+
+
+
 def notify(note, email_address):
-    pass
+
+    subject = 'saythanks.io: someone sent a note!'
+    message = TEMPLATE.format(note.body, note.byline)
+
+    from_address = Email('no-reply@saythanks.io')
+    to_address = Email(email_address)
+    content = Content('text/plain', message)
+
+    mail = Mail(from_address, subject, to_address, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
