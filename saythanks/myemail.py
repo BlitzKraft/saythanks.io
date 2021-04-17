675e27702dd79f6b2c32daa9fe7ca6ffcc8f46a3
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-04-18 00:15:21 +0530

    Fixed #89

--- saythanks/myemail.py
+++ saythanks/myemail.py
@@ -17,7 +17,8 @@ TEMPLATE = """{}
 
 This note of thanks was brought to you by SayThanks.io.
 
-A Kenneth Reitz (me@kennethreitz.org) project.
+Maintained by folks at KGiSL Edu (http://edu.kgisl.com)
+Originally, a Kenneth Reitz project.
 """
 
 
