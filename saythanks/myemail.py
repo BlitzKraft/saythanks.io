08e105263a05f544e75196912fd982255e41fb26
tarunkumark <tkksctwo@gmail.com>
2022-01-23 16:05:26 +0530

    added HTML email option

--- saythanks/myemail.py
+++ saythanks/myemail.py
@@ -10,15 +10,20 @@ from urllib.error import URLError
 API_KEY = os.environ['SENDGRID_API_KEY']
 sg = sendgrid.SendGridAPIClient(api_key=API_KEY)
 
-TEMPLATE = """{}
-
+TEMPLATE = """<div>{}
+<br>
+<br>
 --{}
-
+<br>
+<br>
 =========
-
+<br>
+<br>
 This note of thanks was brought to you by SayThanks.io.
-
+<br>
+<br>
 A KennethReitz project, now maintained by KGiSL Edu (info@kgisl.com).
+</div>
 """
 
 
@@ -32,11 +37,11 @@ def notify(note, email_address):
         who = note.byline or 'someone'
 
         subject = 'saythanks.io: {} sent a note!'.format(who)
-        message = TEMPLATE.format(note.body, note.byline)
-
+        message = note.body
+        message = TEMPLATE.format(note.body,note.byline)
         from_address = Email('no-reply@saythanks.io', name="SayThanks.io")
         to_address = Email(email_address)
-        content = Content('text/plain', message)
+        content = Content('text/html', message)
 
         mail = Mail(from_address, subject, to_address, content)
         response = sg.client.mail.send.post(request_body=mail.get())
