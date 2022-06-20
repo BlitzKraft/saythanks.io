0d5fd3a9412628ba79e19b2d6047cddd2f49f620
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2022-06-20 08:52:05 +0530

    Update main.js
    Changed all `var` to `const` since none of them really change!

--- saythanks/static/js/main.js
+++ saythanks/static/js/main.js
@@ -1,19 +1,19 @@
 $(document).on("change", "#badge-format", function () {
-  var selectedFormat = $("#badge-format").val();
-  var username = $("#username").val();
+  const selectedFormat = $("#badge-format").val();
+  const username = $("#username").val();
   if (selectedFormat === "imageurl") {
     $("#badgeCode").val(
       "https://img.shields.io/badge/Say%20Thanks-!-1EAEDB.svg"
     );
   } else if (selectedFormat === "markdown") {
-    let svg =
+    const svg =
       "[![Say Thanks!](https://img.shields.io/badge/Say%20Thanks-!-1EAEDB.svg)]";
-    let url = `(https://saythanks.io/to/${username})`;
+    const url = `(https://saythanks.io/to/${username})`;
     $("#badgeCode").val(svg + url);
   } else if (selectedFormat === "rst") {
-    let line1 =
+    const line1 =
       ".. image:: https://img.shields.io/badge/Say%20Thanks-!-1EAEDB.svg \n";
-    let line2 = `   :target: https://saythanks.io/to/${username}`;
+    const line2 = `   :target: https://saythanks.io/to/${username}`;
     $("#badgeCode").val(line1 + line2);
   }
 });
