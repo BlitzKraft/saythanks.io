ce8baf66c664991e1501fe425b6f2e16954538fe
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2022-06-20 08:37:27 +0530

    Update main.js
    Refactored to fix #29

--- saythanks/static/js/main.js
+++ saythanks/static/js/main.js
@@ -6,16 +6,14 @@ $(document).on("change", "#badge-format", function () {
       "https://img.shields.io/badge/Say%20Thanks-!-1EAEDB.svg"
     );
   } else if (selectedFormat === "markdown") {
-    $("#badgeCode").val(
-      "[![Say Thanks!](https://img.shields.io/badge/Say%20Thanks-!-1EAEDB.svg)](https://saythanks.io" +
-        "/to/" +
-        username +
-        ")"
-    );
+    let svg = 
+        "[![Say Thanks!](https://img.shields.io/badge/Say%20Thanks-!-1EAEDB.svg)]";
+    let url = `(https://saythanks.io/to/${username})`;
+    $("#badgeCode").val(svg + url);
   } else if (selectedFormat === "rst") {
-    var line1 =
+    let line1 =
       ".. image:: https://img.shields.io/badge/Say%20Thanks-!-1EAEDB.svg \n";
-    var line2 = `   :target: https://saythanks.io/to/${username}`;
+    let line2 = `   :target: https://saythanks.io/to/${username}`;
     $("#badgeCode").val(line1 + line2);
   }
 });
