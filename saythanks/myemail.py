96ac55f64c30285301433148ac8ce982f584f4c5
Ashok B <lifebalance@gmail.com>
2025-05-30 16:16:33 +0530

    Fixed #285 - eliminated the need for querying the DB to get the note UUID

--- saythanks/myemail.py
+++ saythanks/myemail.py
@@ -3,6 +3,7 @@ import os
 import sendgrid
 from sendgrid.helpers.mail import Email, Content, Mail
 from urllib.error import URLError
+from flask import url_for, current_app
 
 # importing module
 import logging
@@ -28,10 +29,13 @@ TEMPLATE = """<div>{}
 --{}
 <br>
 <br>
+The public URL for this note is <a href="{}">here</a>
+<br>
+<br>
 =========
 <br>
 <br>
-This note of thanks was brought to you by SayThanks.io.
+This note of gratitude was brought to you by SayThanks.io.
 <br>
 <br>
 A KennethReitz project, now maintained by KGiSL Edu (info@kgisl.com).
@@ -43,14 +47,24 @@ def notify(note, email_address):
     """Use the note contents and a template, build a
     formatted message. Use sendgrid to deliver the formatted
     message as an email to the user.
+
+    Also include the public URL for the note, so that
+    the user can share it with others.
     """
     try:
+        if not note.uuid:
+            logging.error("Could not find UUID for note — link will be blank.")
+            note_url = ''
+        else:
+            with current_app.app_context():
+                note_url = url_for('share_note', uuid=note.uuid, _external=True)
+
+        
         # Say 'someone' if the byline is empty.
         who = note.byline or 'someone'
 
         subject = 'saythanks.io: {} sent a note!'.format(who)
-        message = note.body
-        message = TEMPLATE.format(note.body, note.byline)
+        message = TEMPLATE.format(note.body, note.byline, note_url)
         from_address = Email('no-reply@saythanks.io', name="SayThanks.io")
         to_address = Email(email_address)
         content = Content('text/html', message)
