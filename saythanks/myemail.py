88d61cdc26bf5a3aecd89b4b68e4036316e751d4
Srimathi Jagadeesan <46951797+Srimathij@users.noreply.github.com>
2025-06-07 21:24:18 +0530

    Update myemail.py

--- saythanks/myemail.py
+++ saythanks/myemail.py
@@ -3,6 +3,7 @@ import os
 import sendgrid
 from sendgrid.helpers.mail import Email, Content, Mail
 from urllib.error import URLError
+from flask import url_for, current_app, request
 
 # importing module
 import logging
@@ -28,13 +29,17 @@ TEMPLATE = """<div>{}
 --{}
 <br>
 <br>
+The public URL for this note is <a href="{}">here</a> <br>
+or {}. 
+<br>
+<br>
 =========
 <br>
 <br>
-This note of thanks was brought to you by SayThanks.io.
+This note of gratitude was brought to you by SayThanks.io.
 <br>
 <br>
-A KennethReitz project, now maintained by KGiSL Edu (info@kgisl.com).
+A KennethReitz project, now maintained by KGiSL Edu (https://edu.kgisl.com).
 </div>
 """
 
@@ -43,14 +48,29 @@ def notify(note, email_address):
     """Use the note contents and a template, build a
     formatted message. Use sendgrid to deliver the formatted
     message as an email to the user.
+
+    Also include the public URL for the note, so that
+    the user can share it with others.
     """
     try:
+        if not note.uuid:
+            logging.error("Could not find UUID for note — link will be blank.")
+            note_url = ''
+        else:
+            with current_app.app_context():
+                note_url = url_for('share_note', uuid=note.uuid, _external=True)
+
+            # server_name = os.environ.get('SERVER_NAME', 'http://localhost:5000')
+            # note_url2 = "https://"+server_name + "/note/" + str(note.uuid) 
+            base_url = request.url_root  
+            note_url2 = base_url + "note/" + str(note.uuid) 
+            logging.error("note_url2: " + note_url2)
+        
         # Say 'someone' if the byline is empty.
         who = note.byline or 'someone'
 
         subject = 'saythanks.io: {} sent a note!'.format(who)
-        message = note.body
-        message = TEMPLATE.format(note.body, note.byline)
+        message = TEMPLATE.format(note.body, note.byline, note_url, note_url2)
         from_address = Email('no-reply@saythanks.io', name="SayThanks.io")
         to_address = Email(email_address)
         content = Content('text/html', message)
@@ -58,7 +78,8 @@ def notify(note, email_address):
         mail = Mail(from_address, subject, to_address, content)
         response = sg.client.mail.send.post(request_body=mail.get())
     except URLError as e:
-        logging.error("URL Error occured "+str(e))
+        logging.error("URL Error occurred "+ str(e))
         print(e)
-    # except Exception as e:
-    #    print(e)
+    except Exception as e:
+        logging.error("General Error occurred: " + str(e))
+        print(e)
