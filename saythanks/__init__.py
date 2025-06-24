2d287c0cabe6c85fb955eddff8a8376bdd67bd7b
Ashok B <lifebalance@gmail.com>
2025-06-24 17:00:44 +0530

    injected the version into all templates

--- saythanks/__init__.py
+++ saythanks/__init__.py
@@ -1,1 +1,8 @@
 from .core import *
+
+# Import your get_version function
+from .version import get_version
+
+@app.context_processor
+def inject_version():
+    return dict(app_version=get_version())
