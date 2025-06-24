2afa470a081cc4e873c2fc422a6b12f85df70c0b
Ashok B <lifebalance@gmail.com>
2025-06-24 21:24:54 +0530

    modified function for #311

--- saythanks/version.py
+++ saythanks/version.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
+
+version_file = "version.txt"
+
+# This function reads the version from a file named "version_file"
+# If the file does not exist, it returns "unknown"
 def get_version():
     try:
-        with open("version.txt") as f:
+        with open(version_file) as f:
             return f.read().strip()
     except FileNotFoundError:
         return "unknown"
