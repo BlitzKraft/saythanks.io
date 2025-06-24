d697c5662072a99a96d42a2bd75b9c1ba40fa169
Ashok B <lifebalance@gmail.com>
2025-06-24 22:10:54 +0530

    eliminate the need for version.txt for #311

--- saythanks/__init__.py
+++ saythanks/__init__.py
@@ -1,8 +1,6 @@
 from .core import *
 
-# Import your get_version function
-from .version import get_version
 
 @app.context_processor
 def inject_version():
-    return dict(app_version=get_version())
+    return dict(app_version=app.config.get('APP_VERSION', 'unknown'))
--- saythanks/version.py
+++ saythanks/version.py
@@ -2,6 +2,7 @@
 
 version_file = "version.txt"
 
+'''
 # This function reads the version from a file named "version_file"
 # If the file does not exist, it returns "unknown"
 def get_version():
@@ -10,4 +11,22 @@ def get_version():
             return f.read().strip()
     except FileNotFoundError:
         return "unknown"
+'''
 
+#####################
+## Get Version details
+######################
+import subprocess as commands
+
+def get_version():
+
+    status, version = commands.getstatusoutput ("git describe --tags --long")
+    if not status: 
+        print ("Version: " + version)
+    else: 
+        print("git describe returned bad status!")
+        print("The repo should have at least one release tag!")
+        print("Please see https://help.github.com/articles/creating-releases/")
+        version = "unknown"
+
+    return version 
