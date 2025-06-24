9f172080e11b1e3dc8d84aada2790d99f785822f
Ashok B <lifebalance@gmail.com>
2025-06-24 22:25:25 +0530

    minor edits after #311

--- saythanks/version.py
+++ saythanks/version.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# This file is part of the SayThanks project.
+# It is used to retrieve the current version of the project.
+import subprocess as commands
+
 version_file = "version.txt"
 
 '''
@@ -14,16 +18,21 @@ def get_version():
 '''
 
 #####################
-## Get Version details
+# Get Version details
 ######################
-import subprocess as commands
+
 
 def get_version():
+    """
+    Retrieve the current version of the project using git tags.
 
-    status, version = commands.getstatusoutput ("git describe --tags --long")
+    Returns:
+        str: The version string if available, otherwise "unknown".
+    """
+    status, version = commands.getstatusoutput("git describe --tags --long")
     if not status: 
-        print ("Version: " + version)
-    else: 
+        print("Version: " + version)
+    else:
         print("git describe returned bad status!")
         print("The repo should have at least one release tag!")
         print("Please see https://help.github.com/articles/creating-releases/")
