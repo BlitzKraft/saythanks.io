c358264518b39e52a33493e8b98e42b7f34c81ba
Jeffrey Shalom <jeffshalom06@gmail.com>
2026-09-21 09:19:57 +0530

    fix(ui): add fallbacks for user name and avatar when oauth fields are missing

--- saythanks/core.py
+++ saythanks/core.py
@@ -647,8 +647,8 @@ def callback_handling():
                 'AUTH0_JWT_V2_TOKEN having expired.'
             ),
         )
-    picture = user_detail_info.get('picture')
-    name = user_detail_info.get('name')
+    picture = user_detail_info.get('picture') or user_info.get('picture') or url_for('static', filename='images/inbox.png')
+    name = user_detail_info.get('name') or user_info.get('name') or nickname
     session['profile']['nickname'] = nickname
     session['profile']['picture'] = picture
     session['profile']['name'] = name
